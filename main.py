0c43d6906244e2a6269e9f49c07ab98054bff2c0
Albert <albert.asanov.69@gmail.com>
2025-03-19 06:17:54 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print(5)
+# version 0.1.0
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from config import Token
+
+bot = Bot(token='8137179834:AAGziUD4pfa5cqyw55Fp_UH3VACo7nWQ-0A')
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def process_start_command(message):
+    await message.answer(f"Привет, {message.from_user.full_name} !")
+
+@dp.message()
+async def echo_handler(message):
+    try:
+        await message.answer(message.text)
+    except:
+        await message.answer("Попробуй снова!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
