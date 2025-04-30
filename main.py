bf545f28b6f4bf3feee124c21459f8811bdaa334
Varddd11 <heromerobrine@gmail.com>
2025-04-30 08:27:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,13 @@ from handlers import handlers, callbacks
 import sys
 from utils import setup_logger
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-dp.include_router(handlers.r)
-dp.include_router(callbacks.r)
-dp.startup.register(set_command_list)
-
 
 async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    dp.include_router(handlers.r)
+    dp.include_router(callbacks.r)
+    dp.startup.register(set_command_list)
 
     #logging.basicConfig(level=logging.INFO)
     setup_logger(fname=__name__)
